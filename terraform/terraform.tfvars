275421d246fa5ef6befa46aa4515b63465d533b1
Or Atar <Or_At@imisystems.com>
2024-02-18 08:20:17 +0200

    update project files

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,19 @@
+api_gateway_name = "lambda_api_gateway"
+
+api_gateway_path = "webhook"
+
+api_gateway_stage_name = "prod"
+
+lambda_policy_name = "LambdaPolicy"
+
+lambda_role_name = "LambdaRole"
+
+lambda_function_name = "logging-lambda"
+
+lambda_source_zip = "../lambda_source/deployment_package.zip"
+
+s3_bucket_name = "git-logging-bucket"
+
+repo_name = "project_repo"
+
+webhook_name = "api_gateway_webhook"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,36 @@
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket"
+
+variable "api_gateway_name" {
+  description = "Api Gateway name"
 }
 
 variable "api_gateway_path" {
   description = "Api Gateway path"
 }
 
-variable "api_gateway_name" {
-  description = "Api Gateway name"
+variable "api_gateway_stage_name" {
+  description = "Api gateway stage name"
 }
 
-variable "lambda_function_name" {
-  description = "Lambda function name"
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+}
+
+variable "lambda_policy_name" {
+  description = "Lambda policy name"
 }
 
 variable "lambda_role_name" {
   description = "Lambda role name"
 }
 
-variable "api_gateway_stage_name" {
-  description = "Api gateway stage name"
+
+variable "lambda_function_name" {
+  description = "Lambda function name"
 }
+
+
 variable "lambda_source_zip" {
   description = "Lambda source zip file"
-}
-variable "lambda_policy_name" {
-  description = "Lambda policy name"
 }
 
 variable "github_token" {
@@ -37,9 +42,8 @@ variable "repo_name" {
   description = "github repository name"
 }
 
+
 variable "webhook_name" {
   description = "github webhook name"
 }
-
-
 
